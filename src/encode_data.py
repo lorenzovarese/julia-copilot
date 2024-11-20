03f6722ff187261eb5ae63923c852efbcdd16301
Karma Riuk <riukkarma@gmail.com>
2024-11-20 11:29:16 +0100

    encoding the data and training the model

---
+++ src/encode_data.py
@@ -0,0 +1,94 @@
+from transformers import AutoTokenizer
+import pandas as pd
+import os, multiprocessing
+import datasets
+import argparse
+from pandarallel import pandarallel  # For parallel processing
+NUM_PROC = min(50, multiprocessing.cpu_count() - 1)
+pandarallel.initialize(progress_bar=True, nb_workers=NUM_PROC)
+
+MODEL_NAME = "HuggingFaceTB/SmolLM-135M"
+TOKENIZER = AutoTokenizer.from_pretrained(MODEL_NAME)
+TOKENIZER.pad_token = TOKENIZER.eos_token
+
+CONTEXT_LENGTH = 2048
+
+def encode_data(
+        encoded_data_path=os.path.join("data", f"encoded_data"),
+        data_path=os.path.join("data", "combined_projects.zip"), 
+        frac_of_data=1,
+        force=False,
+        verbose=False,
+    ):
+    if not force and os.path.exists(encoded_data_path):
+        if verbose: print(f"Loading encoded data from '{encoded_data_path}'...")
+        return datasets.load_from_disk(encoded_data_path)
+    
+    if verbose: 
+        if force: print("Forcing re-encoding of data.")
+        else: print(f"No cached data found at {encoded_data_path}.")
+        print(f"Loading data from '{data_path}'...")
+
+    projects = pd.read_json(data_path)
+
+    if verbose: print("Extracting functions...")
+    functions = pd.DataFrame([func for funcs in projects.functions for func in funcs])
+
+    if verbose: print("Dropping duplicates...")
+    functions = functions.drop_duplicates(subset=["documentation", "signature", "body"])
+    if frac_of_data < 1:
+        functions = functions.sample(frac=frac_of_data, random_state=100)
+
+    if verbose: print("Creating the actual functions...")
+    functions["complete_function"] = \
+        "### DOCUMENTATION ### " + "\n" \
+        + functions["documentation"] + "\n" \
+        + "### CODE ### " + "\n" \
+        + functions["signature"] + "\n" \
+        + functions["body"] + "\n" \
+        + "end"
+
+    if verbose: print("Filtering functions that are longer than the context length but have at least some documentation...")
+    def filter_function(function):
+        return (
+            function['type'] == "basic"
+            and len(TOKENIZER(function['documentation'])['input_ids']) > 10 
+            and len(TOKENIZER(function["complete_function"])['input_ids']) <= CONTEXT_LENGTH
+        )
+
+    len_before = len(functions)
+    functions = functions[functions.parallel_apply(filter_function, axis=1)]
+    print(f"Filtered out {len_before - len(functions)} functions ({len(functions)} remaining).")
+
+
+    if verbose: print("Encoding data...")
+    def encode_function(function):
+        tokenized_inputs = TOKENIZER(
+            function["complete_function"],
+            truncation=True,
+            max_length=CONTEXT_LENGTH,
+            padding="max_length"
+        )
+        tokenized_inputs['labels'] = tokenized_inputs['input_ids'].copy()
+        return tokenized_inputs
+    dataset = datasets.Dataset.from_pandas(functions)
+    dataset = dataset.map(encode_function, batched=True, num_proc=NUM_PROC)
+
+    if verbose: print(f"Saving encoded data to '{encoded_data_path}'...")
+    dataset.save_to_disk(encoded_data_path)
+    return dataset
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Encode the dataset for training a model.")
+    parser.add_argument("-f", "--force", action="store_true", help="Force re-encoding of data.")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Print verbose output.")
+    parser.add_argument("--encoded-data-path", type=str, default=os.path.join("data", "encoded_data"), help="Path to save the encoded dataset. Default is 'data/encoded_data'. Note: The path is then extended with the fraction of the data, together with whether it is only recent issues or not.")
+    parser.add_argument("--frac", type=float, default=1, help="Fraction of the data to use. Default is 1.")
+
+    args = parser.parse_args()
+    encode_data(
+        encoded_data_path=args.encoded_data_path,
+        force=args.force,
+        verbose=args.verbose,
+        frac_of_data=args.frac,
+    )
---
+++ src/eval.py
@@ -0,0 +1,69 @@
+import argparse, os
+import torch
+from encode_data import encode_data, TOKENIZER, MODEL_NAME
+
+from transformers import AutoModelForCausalLM
+from transformers import TrainingArguments, Trainer
+
+
+
+def trainer_for_model(model, dataset, output_dir=os.path.join("data", "checkpoints")):
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    args = TrainingArguments(
+        save_strategy="epoch",
+        output_dir=output_dir,
+        overwrite_output_dir=False,
+        learning_rate=2e-5,
+        per_device_train_batch_size=2,
+        num_train_epochs=5,
+        fp16=True,
+    )
+
+    trainer = Trainer(
+        model,
+        args,
+        train_dataset=dataset,
+        tokenizer=TOKENIZER,
+        # compute_metrics=compute_metrics,
+    )
+
+    return trainer
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--train-model", action="store_true", help="Force the training of the model.")
+    parser.add_argument("--frac-of-data", type=float, default=1, help="Fraction of data to use for training. Default is 1. Use a smaller value (between 0 and 1) for testing.")
+    parser.add_argument("--encoded-data-path", type=str, default=os.path.join("data", "encoded_data"), help="Path to the encoded dataset. Default is 'data/encoded_data'. Note: The path is then extended with the fraction of the data, together with whether it is only recent issues or not.")
+
+    args = parser.parse_args()
+
+    encoded_dataset = encode_data(
+        encoded_data_path=args.encoded_data_path,
+        frac_of_data=args.frac_of_data,
+        verbose=True,
+    )
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    if args.train_model:
+        model = AutoModelForCausalLM.from_pretrained(MODEL_NAME).to(device)
+    else:
+        if args.checkpoint:
+            model = AutoModelForCausalLM.from_pretrained(args.checkpoint)
+        else:
+            raise ValueError("You need to provide a checkpoint path if you're not forcing training")
+
+    trainer = trainer_for_model(model, encoded_dataset)
+
+    # for param in model.model.layers[:20].parameters():
+    #     param.requires_grad = False
+    print(f"num params:", model.num_parameters())
+    print(f"num trainable params:", model.num_parameters(only_trainable=True))
+    if args.train_model:
+        trainer.train()
+        save_path = os.path.join("data", "best-model")
+        trainer.model.save_pretrained(save_path)
+        trainer.tokenizer.save_pretrained(save_path)
