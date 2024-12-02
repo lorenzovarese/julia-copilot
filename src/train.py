8fd3bed0f1c2c17132fbfc176dfe4cdd3f3fa385
Karma Riuk <riukkarma@gmail.com>
2024-12-02 13:23:41 +0100

    added the possibility to quantize models

--- src/train.py
+++ src/train.py
@@ -6,9 +6,56 @@ from encode_data import encode_data, setup_tokenizer
 from transformers import AutoModelForCausalLM
 from transformers import TrainingArguments, Trainer
 
+from transformers import BitsAndBytesConfig
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
+from trl import SFTTrainer
+
 
 if "CUDA_VISIBLE_DEVICES" not in os.environ:
     os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
+def trainer_for_quantized_model(model_name, dataset, batch_size=2, output_dir="checkpoints"):
+    tokenizer, _ = setup_tokenizer(model_name)
+
+    nf4_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_compute_dtype=torch.float16 # P.S. use torch.float16 on gym J
+    )
+    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", quantization_config=nf4_config)
+    r = 8
+    lora_config = LoraConfig(
+        task_type="CAUSAL_LM",
+        r=r,
+        lora_alpha=2*r
+    )
+
+    model = prepare_model_for_kbit_training(model)
+    model.gradient_checkpointing_enable()
+
+    model = get_peft_model(model, lora_config)
+    model.print_trainable_parameters()
+
+    args = TrainingArguments(
+        save_strategy="epoch",
+        output_dir=output_dir,
+        overwrite_output_dir=True,
+        learning_rate=2e-5,
+        per_device_train_batch_size=batch_size,
+        num_train_epochs=5,
+        fp16=True,
+        optim="adamw_8bit",
+    )
+
+    trainer = SFTTrainer(
+        model=model,
+        args=args,
+        train_dataset=dataset,
+        peft_config=lora_config,
+        tokenizer=tokenizer
+    )
+    return trainer
 
 def trainer_for_model(model_name, dataset, batch_size=2, output_dir="checkpoints"):
     model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
@@ -36,6 +83,7 @@ def trainer_for_model(model_name, dataset, batch_size=2, output_dir="checkpoints
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--model", type=str, default="HuggingFaceTB/SmolLM-135M", help="Model to use for training. Default is 'HuggingFaceTB/SmolLM-135M'.")
+    parser.add_argument("-q", "--quantized", action="store_true", help="Quantize the model using BitsAndBytes.")
     parser.add_argument("--first-line", action="store_true", help="Extract only the first line of the documentation.")
     parser.add_argument("--frac-of-data", type=float, default=1, help="Fraction of data to use for training. Default is 1. Use a smaller value (between 0 and 1) for testing.")
     parser.add_argument("-b", "--batch-size", type=int, default=2, help="Batch size for training. Default is 2. Increase this if you have a larger GPU.")
@@ -62,7 +110,10 @@ if __name__ == "__main__":
         output_dir += "_first_line"
 
     print(f"Saving checkpoints to '{output_dir}'")
-    trainer = trainer_for_model(args.model, encoded_dataset, batch_size=args.batch_size, output_dir=output_dir)
+    if args.quantized:
+        trainer = trainer_for_quantized_model(args.model, encoded_dataset, batch_size=args.batch_size, output_dir=output_dir)
+    else:
+        trainer = trainer_for_model(args.model, encoded_dataset, batch_size=args.batch_size, output_dir=output_dir)
 
     # for param in model.model.layers[:20].parameters():
     #     param.requires_grad = False
